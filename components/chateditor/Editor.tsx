8c22ec98e14de067e91ced8e994aaae9e58c602f
Sanghyuk Seo <103694448+sseo1124@users.noreply.github.com>
2025-10-02 10:43:35 +0900

    fix: 서버가 아닌 프론트단에서 initialValue를 편집기 값에 넣어주기

--- components/chateditor/Editor.tsx
+++ components/chateditor/Editor.tsx
@@ -1,39 +1,73 @@
-import React, { useState, useEffect, useMemo } from "react";
-import { createEditor, Descendant } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
-import { withHistory } from "slate-history";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { createEditor, Descendant, Editor, Transforms } from "slate";
+import { Editable, Slate, withReact } from "slate-react";
 import * as Y from "yjs";
-import { slateNodesToInsertDelta } from "@slate-yjs/core";
+import { withYjs, YjsEditor, withYHistory } from "@slate-yjs/core";
+import { HocuspocusProvider } from "@hocuspocus/provider";
 
-const initialValue: Descendant[] = [
+const initialValue = [
   {
     type: "paragraph",
-    children: [
-      { text: "This is editable plain text, just like a <textarea>!" },
-    ],
+    children: [{ text: "" }],
   },
 ];
 
 export default function CollaborativeEditor() {
-  const sharedType = useMemo(() => {
-    const yDoc = new Y.Doc();
-    const sharedType = yDoc.get("content", Y.XmlText);
-
-    sharedType.applyDelta(slateNodesToInsertDelta(initialValue));
-
-    return sharedType;
-  }, []);
-
-  return <PlainTextEditor />;
-}
-
-function PlainTextEditor() {
-  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
   const [value, setValue] = useState([]);
 
+  const provider = useMemo(
+    () =>
+      new HocuspocusProvider({
+        url: "ws://127.0.0.1:8000/collaboration",
+        name: "slate-yjs-demo",
+        connect: false,
+      }),
+    []
+  );
+
+  const editor = useMemo(() => {
+    const sharedType = provider.document.get("content", Y.XmlText);
+    const e = withReact(withYHistory(withYjs(createEditor(), sharedType)));
+
+    const { normalizeNode } = e;
+    e.normalizeNode = (entry) => {
+      const [node] = entry;
+
+      if (!Editor.isEditor(node) || node.children.length > 0) {
+        return normalizeNode(entry);
+      }
+
+      Transforms.insertNodes(
+        editor,
+        {
+          type: "paragraph",
+          children: [{ text: "" }],
+        },
+        { at: [0] }
+      );
+    };
+
+    return e;
+  }, [provider.document]);
+
+  useEffect(() => {
+    provider.connect();
+    return () => provider.disconnect();
+  }, [provider]);
+
+  useEffect(() => {
+    YjsEditor.connect(editor);
+    return () => YjsEditor.disconnect(editor);
+  }, [editor]);
+
   return (
-    <Slate editor={editor} value={value} onChange={setValue}>
-      <Editable placeholder="Enter some plain text..." />
+    <Slate
+      value={value}
+      onChange={setValue}
+      editor={editor}
+      initialValue={initialValue}
+    >
+      <Editable />
     </Slate>
   );
 }
