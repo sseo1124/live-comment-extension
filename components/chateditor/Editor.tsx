9fa88cebabf5c969a2ad458aabe7249cad593864
Sanghyuk Seo <103694448+sseo1124@users.noreply.github.com>
2025-10-02 09:33:04 +0900

    feat: slate 편집기와 yjs와 연결 셋팅

--- components/chateditor/Editor.tsx
+++ components/chateditor/Editor.tsx
@@ -1,16 +1,9 @@
-import React, { useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { createEditor, Descendant } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 import { withHistory } from "slate-history";
-
-const PlainTextEditor = () => {
-  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
-  return (
-    <Slate editor={editor} initialValue={initialValue}>
-      <Editable placeholder="Enter some plain text..." />
-    </Slate>
-  );
-};
+import * as Y from "yjs";
+import { slateNodesToInsertDelta } from "@slate-yjs/core";
 
 const initialValue: Descendant[] = [
   {
@@ -21,4 +14,26 @@ const initialValue: Descendant[] = [
   },
 ];
 
-export default PlainTextEditor;
+export default function CollaborativeEditor() {
+  const sharedType = useMemo(() => {
+    const yDoc = new Y.Doc();
+    const sharedType = yDoc.get("content", Y.XmlText);
+
+    sharedType.applyDelta(slateNodesToInsertDelta(initialValue));
+
+    return sharedType;
+  }, []);
+
+  return <PlainTextEditor />;
+}
+
+function PlainTextEditor() {
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  const [value, setValue] = useState([]);
+
+  return (
+    <Slate editor={editor} value={value} onChange={setValue}>
+      <Editable placeholder="Enter some plain text..." />
+    </Slate>
+  );
+}
